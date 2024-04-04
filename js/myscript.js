52db7d12aa8133a14bb19f3db3f0df89ec52b0b5
moran <ooklibaioo@gmail.com>
2024-04-04 16:43:32 +0800

    img

--- js/myscript.js
+++ js/myscript.js
@@ -1,26 +1,28 @@
 window.onload = function () {
     //video_list
     var video_list = [
-        "Tn8IDh21AVY",
-        "3clqk2U3T9Y",
-        "vxo2o92Zc3o",
-        "lHZ2azw5LCc",
-        "NHHLCVom684",
-        "v0vvD2bUNZ0",
-        "yuFwErfsnTM",
-        "ChKGbkLOokA",
-        "pVsZ9ti6vdc",
-        "uqlmazoOCmw",
-        "9vak1ZGEUu8",
-        "YAZOCtLMNmI",
+        "GXlckaGr0Eo",
+        "wM7eMJ26kc8",
+        "XyGcUXDuXVY",
+        "jKUBYGwoW3A",
+        "NDgTwlQSzMM",
+        "PQ99nu4cq1k",
+        "tqrqiyPC14Q",
+        "y0wXQ5EdW_0",
+        "66GYEALFuDs",
+        "uiq3NKEnqEg"
     ];
 
     var study_list = [
-        "mPZkdNFkNps",
+        "ZEbCz7B2-Eg",
         "jfKfPfyJRdk",
-        "Ds7Gnig9TjA",
-        "O6MDJZGtUUE",
-        "mt87ltDUzVI"
+        "sUwD3GRPJos",
+        "82Wx5Ou6KY8",
+        "feTTjwiZE20",
+        "DcvvWjExea4",
+        "BTMjD7_evjE",
+        "Dg0IjOzopYU",
+        "9OPXTEORZZ8"
     ];
 
 
@@ -39,12 +41,12 @@ window.onload = function () {
             scroll_top = document.documentElement.scrollTop //獲取已滾動的高度
             for (i = 0; i < IdList.length; i++) {
                 var targetID = document.getElementById(IdList[i])
-                var targetOffsetTop = targetID.offsetTop - 25 //獲取目標ID與網頁頂部之間的高度 - 25
+                var targetOffsetTop = targetID.offsetTop - 50 //獲取目標ID與網頁頂部之間的高度 - 25
                 var targetOffsetTop2 = ""
                 if (i >= IdList.length - 1) {
                     targetOffsetTop2 = scroll_height
                 } else {
-                    targetOffsetTop2 = document.getElementById(IdList[i + 1]).offsetTop - 25
+                    targetOffsetTop2 = document.getElementById(IdList[i + 1]).offsetTop - 50
                 }
                 if (scroll_top >= targetOffsetTop && scroll_top < targetOffsetTop2) {
                     document.getElementById(menulist[i]).style.backgroundColor = "white"
@@ -57,20 +59,20 @@ window.onload = function () {
         })
 
         //放入標簽並從Array獲取專輯圖片
-        for (i = 0; i < 12; i++) {
+        for (i = 0; i < 9; i++) {
             document.getElementById("relax-list").innerHTML += '<a href = "https://www.youtube.com/watch?v=' + video_list[i] + '"><div class="relax_data"><div class="relax_img"></div><p class="relax_name"></p></div></a>'
             var relax_image = document.getElementsByClassName("relax_img")[i]
             var relax_name = document.getElementsByClassName("relax_name")[i]
-            relax_image.style.backgroundImage = "url(https://img.youtube.com/vi/" + video_list[i] + "/mqdefault.jpg)"
+            relax_image.style.backgroundImage = "url(./img/ASMR/" + video_list[i] + ".jpg)"
             relax_name.innerHTML = video_list[i]
         }
 
         //放入標簽並從Array獲取數據
-        for (k = 0; k < 12; k++) {
+        for (k = 0; k < 9; k++) {
             document.getElementById("study-list").innerHTML += '<a href = "https://www.youtube.com/watch?v=' + study_list[k] + '"><div class="study_data"><div class="study_img"></div><p class="study_name"></p></div></a>'
             var study_image = document.getElementsByClassName("study_img")[k]
             var study_name = document.getElementsByClassName("study_name")[k]
-            study_image.style.backgroundImage = "url(https://img.youtube.com/vi/" + study_list[k] + "/mqdefault.jpg)"
+            study_image.style.backgroundImage = "url(./img/STUDY/" + study_list[k] + ".jpg)"
             study_name.innerHTML = study_list[k]
         }
 
